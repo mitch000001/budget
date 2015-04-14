9cbd98d6bc141636e5120cd809968278744dfb45
Michael Wagner <mitch.wagna@gmail.com>
2015-04-14 23:02:14 +0200

    Add balance and sum to index

--- budget.go
+++ budget.go
@@ -8,11 +8,32 @@ func NewBudget() *Budget {
 }
 
 type Budget struct {
-	Einnahmen map[string]float64
-	Ausgaben  map[string]float64
+	Einnahmen Earnings
+	Ausgaben  Expenses
 }
 
 func (b *Budget) Balance() float64 {
-	var balance float64
-	return balance
+	earnings := b.Einnahmen.Sum()
+	expenses := b.Ausgaben.Sum()
+	return earnings - expenses
+}
+
+type Earnings map[string]float64
+
+func (e *Earnings) Sum() float64 {
+	sum := 0.0
+	for _, val := range *e {
+		sum += val
+	}
+	return sum
+}
+
+type Expenses map[string]float64
+
+func (e *Expenses) Sum() float64 {
+	sum := 0.0
+	for _, val := range *e {
+		sum += val
+	}
+	return sum
 }
